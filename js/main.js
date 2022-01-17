b60eab94f5028094eb9fc647238b7fbde3236ba5
EddyBautista <bautista.j.eddy@gmail.com>
2022-01-16 20:19:38 -0600

    set up main.js

---
+++ js/main.js
@@ -0,0 +1,5 @@
+import * as THREE from '../node_modules/three/build/three.module'
+
+// module used to allow click and drag functionality 
+import { TrackballControls } from '../node_modules/three/examples/jsm/controls/TrackballControls'
+
